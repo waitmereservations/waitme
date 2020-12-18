2e8f9d293513807e876c0efd9fc2aba7aba92b1c
caliguy2322 <60902909+caliguy2322@users.noreply.github.com>
2020-12-18 10:58:00 -0800

    added seeding

--- db/mongo/seed.js
+++ db/mongo/seed.js
@@ -1,15 +1,35 @@
-db = db.getSiblingDB("test");
-db.article.drop();
+db = db.getSiblingDB("waitme");
+db.tables.drop();
+db.tables.save( {table_number : "Table 1" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "Table 2" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "Table 3" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "Table 4" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "Table 5" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "Table 6" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "Table 7" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "Table 8" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "Table 9" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "Table 10" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "Table 11" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "Table 12" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "waitlist" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
+db.tables.save( {table_number : "reservation" , capacity : 4 , createdAt : new Date(1079895594000) , updatedAt : new Date(1079895594000) , parties : null ,table_status : null });
 
-db.article.save( {
-    title : "this is my title" , 
-    author : "bob" , 
-    posted : new Date(1079895594000) , 
-    pageViews : 5 , 
-    tags : [ "fun" , "good" , "fun" ] ,
-    comments : [ 
-        { author :"joe" , text : "this is cool" } , 
-        { author :"sam" , text : "this is bad" } 
-    ],
-    other : { foo : 5 }
-});
+db.party_statuses.drop();
+db.party_statuses.save( {name : "upcoming"} );
+db.party_statuses.save( {name : "seated"} );
+db.party_statuses.save( {name : "left"} );
+
+db.party_types.drop();
+db.party_types.save( {name : "walkin"} );
+db.party_types.save( {name : "reservation"} );
+db.party_types.save( {name : "waitlist"} );
+
+db.table_statuses.drop();
+db.table_statuses.save( {name : "seated"} );
+db.table_statuses.save( {name : "appetizer"} );
+db.table_statuses.save( {name : "entree"} );
+db.table_statuses.save( {name : "dessert"} );
+db.table_statuses.save( {name : "bill"} );
+db.table_statuses.save( {name : "left"} );
+db.table_statuses.save( {name : "queued"} );
